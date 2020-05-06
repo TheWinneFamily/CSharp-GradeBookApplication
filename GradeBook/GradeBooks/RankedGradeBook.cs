1de94b2127235514ff81e412a987a4cfd1a8a562
Scott Winne <thewinnefamily@yahoo.com>
2020-05-06 08:18:37 -0400

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using GradeBook.Enums;
 using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +9,33 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            double twentyPercentOfCount = Students.Count / 5;
+            int studentsWithHigherGrades = Students.Count(s => s.AverageGrade > averageGrade);
+            if (studentsWithHigherGrades < twentyPercentOfCount)
+            {
+                return 'A';
+            }
+            else if (studentsWithHigherGrades < twentyPercentOfCount * 2)
+            {
+                return 'B';
+            }
+            else if (studentsWithHigherGrades < twentyPercentOfCount * 3)
+            {
+                return 'C';
+            }
+            else if (studentsWithHigherGrades < twentyPercentOfCount * 4)
+            {
+                return 'D';
+            }
+            return 'F';
         }
     }
 }
